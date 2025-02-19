2dfb4da56f9d6583334f9f54def468df09f9bb97
Alessandro Martins dos Santos <alessandromartinsdosantos@gmail.com>
2025-02-18 21:52:56 -0300

    18/02

---
+++ Fundamentos de programação com Python/Exemplos/funcao.py
@@ -0,0 +1,7 @@
+def soma():
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o Segundo número: "))
+    total = num1 + num2
+    print("A soma é ", total)
+
+soma()
---
+++ Fundamentos de programação com Python/Exemplos/funcao2.py
@@ -0,0 +1,10 @@
+def soma(num1, num2):
+    total = num1 + num2
+    return total
+
+n1 = float(input("Digite o primeiro número: "))
+n2 = float(input("Digite o Segundo número: "))
+
+resultado = soma(n1, n2)
+
+print("A soma é ", resultado)
